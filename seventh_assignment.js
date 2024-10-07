130d130a34b3b7541aac6cd7c32276c92fb68658
Zahra <z.someh@gmail.com>
2024-10-07 12:34:46 +0200

    finish all of exercises 1 to 7

--- seventh_assignment.js
+++ seventh_assignment.js
@@ -1,9 +1,48 @@
 //DOM manipulation:
 //ex 1:
-
+function changeText(){
+    const myElement=document.getElementById("change");
+    myElement.textContent="Leran it very well :)";
+}
+   
+   
 //ex 2:
+function addItem(){
+    var ulList=document.getElementById("myList")
+    var liList=document.createElement("li");
+    var newItem=window.prompt(" Enter new item");
+    if(newItem){
+        liList.textContent=newItem;
+        ulList.appendChild(liList);
 
+    }else{
+        window.alert("please add a item");
+    }
+
+}
 
 //ex 3:
+function changeImage(){
+    const image=document.getElementById("img1")
+    if(image.src.includes("image1")){
+        image.src="img/flower2.jpg?text=image2";
+    }else{
+        image.src="img/flower1.jpeg?text=image1";
+    }
+
+}
 
 //ex 4:
+function validateForm(){
+    const username=document.getElementById('username').value;
+    const password=document.getElementById('password').value;
+    
+    const correctUsername='zahra';
+    const correctPassword='123456';
+    
+    if(username===correctUsername && password===correctPassword){
+        window.alert('login success')
+    }else{
+        window.alert('Invalid login')
+}
+}
